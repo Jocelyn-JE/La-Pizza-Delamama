7bac168b2480e96bcc3ce5bd7e1a44e254f497a5
Samuel Giret <samuel.giret@epitech.eu>
2025-05-28 13:28:03 +0200

    ✨ Add Cook class implementation and integrate into Makefile

---
+++ src/plazza/kitchen/cook/Cook.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2025
+** La-Pizza-Delamama
+** File description:
+** Cook
+*/
+
+#ifndef COOK_HPP_
+#define COOK_HPP_
+
+#include "../../Pizza.hpp"
+
+namespace plazza {
+
+class Kitchen;
+
+class Cook {
+    public:
+        plazza::Kitchen &kitchen;
+        bool working;
+        plazza::Pizza pizza;
+
+        Cook(plazza::Kitchen &kitchen);
+        void cook();
+        // void dump();
+};
+}  // namespace plazza
+
+#endif /* !COOK_HPP_ */
